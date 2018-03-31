ec168b5d0f8a6b6ac1a2adcb4febd641b70cf9db
shan1024 <shanm@wso2.com>
2018-03-31 21:48:35 +0530

    Fix issue in record literal

--- src/main/gen/org/ballerinalang/plugins/idea/parser/BallerinaParserUtil.java
+++ src/main/gen/org/ballerinalang/plugins/idea/parser/BallerinaParserUtil.java
@@ -28,52 +28,59 @@ public class BallerinaParserUtil extends GeneratedParserUtilBase {
 
     public static boolean isPackageExpected(PsiBuilder builder, int level) {
         IElementType next1Element = builder.lookAhead(1);
-        if (next1Element != null && next1Element.toString().equals(":")) {
-            IElementType next2Element = builder.lookAhead(2);
-            if (next2Element != null && next2Element == BallerinaTypes.IDENTIFIER) {
-                IElementType next3Element = builder.lookAhead(3);
-                // The next token can be one of the following tokens. Right brace is to check in record key literals.
-                // Comma is used for record literals in function invocations - test(a,{b:c, d:e})
-                // Left bracket is used to identify array elements - {name:"Ballerina", address:args[0]};
-                if (next3Element != null && (next3Element == BallerinaTypes.SEMICOLON
-                        || next3Element == BallerinaTypes.COLON || next3Element == BallerinaTypes.RIGHT_PARENTHESIS
-                        || next3Element == BallerinaTypes.RIGHT_BRACE || next3Element == BallerinaTypes.COMMA
-                        || next3Element == BallerinaTypes.LEFT_BRACKET
-                        || next3Element == BallerinaTypes.LEFT_PARENTHESIS)) {
-                    // We need to look behind few steps to identify the last token. If this token is not "?" only we
-                    // identify that the package is required.
-                    int steps = -1;
-                    IElementType rawLookup;
+        if (next1Element == null || !next1Element.toString().equals(":")) {
+            return true;
+        }
+        IElementType next2Element = builder.lookAhead(2);
+        if (next2Element == null || next2Element != BallerinaTypes.IDENTIFIER) {
+            return true;
+        }
+        IElementType next3Element = builder.lookAhead(3);
+        // The next token can be one of the following tokens. Right brace is to check in record key literals.
+        // Comma is used for record literals in function invocations - test(a,{b:c, d:e})
+        // Left bracket is used to identify array elements - {name:"Ballerina", address:args[0]};
+        if (next3Element != null && (next3Element == BallerinaTypes.SEMICOLON
+                || next3Element == BallerinaTypes.COLON || next3Element == BallerinaTypes.RIGHT_PARENTHESIS
+                || next3Element == BallerinaTypes.RIGHT_BRACE || next3Element == BallerinaTypes.COMMA
+                || next3Element == BallerinaTypes.LEFT_BRACKET
+                || next3Element == BallerinaTypes.LEFT_PARENTHESIS)) {
+            // We need to look behind few steps to identify the last token. If this token is not "?" only we
+            // identify that the package is required.
+            int steps = -1;
+            IElementType rawLookup;
+            do {
+                rawLookup = builder.rawLookup(steps--);
+                if (isWhiteSpaceOrComment(rawLookup)) {
+                    continue;
+                }
+                // Left brace is to check in record key literals. Comma and other token is checked for situations like -
+                // {name:"Child", parent:parent}
+                if (rawLookup == BallerinaTypes.QUESTION_MARK || rawLookup == BallerinaTypes.LEFT_BRACE
+                        || (rawLookup == BallerinaTypes.COMMA
+                        && (next3Element == BallerinaTypes.RIGHT_BRACE || next3Element == BallerinaTypes.COMMA)
+                )) {
+                    // Note - Another raw lookup is added for situations like below. Second record key value
+                    // pair does not get identified correctly otherwise.
+                    // {sqlType:sql:Type.INTEGER, value:xmlDataArray};
+                    IElementType rawLookup2 = builder.rawLookup(steps - 1);
                     do {
-                        rawLookup = builder.rawLookup(steps--);
                         if (isWhiteSpaceOrComment(rawLookup)) {
                             continue;
                         }
-                        // Left brace is to check in record key literals.
-                        if (rawLookup == BallerinaTypes.QUESTION_MARK || rawLookup == BallerinaTypes.LEFT_BRACE
-                                || (rawLookup == BallerinaTypes.COMMA && next3Element == BallerinaTypes.RIGHT_BRACE)) {
-                            // Note - Another raw lookup is added for situations like below. Second record key value
-                            // pair does not get identified correctly otherwise.
-                            // {sqlType:sql:Type.INTEGER, value:xmlDataArray};
-                            IElementType rawLookup2 = builder.rawLookup(steps - 1);
-                            do {
-                                if (isWhiteSpaceOrComment(rawLookup)) {
-                                    continue;
-                                }
-                                if (rawLookup2 != BallerinaTypes.ASSIGN && rawLookup2 != BallerinaTypes.COLON
-                                        && rawLookup2 != BallerinaTypes.DOT) {
-                                    return true;
-                                } else {
-                                    return false;
-                                }
-                            } while (rawLookup2 != null && isWhiteSpaceOrComment(rawLookup2));
-
-                        } else {
+                        // Quoted literal string example - {address:{"city":"Colombo", "country":"SriLanka"},
+                        //info:info}
+                        if (rawLookup2 != BallerinaTypes.ASSIGN && rawLookup2 != BallerinaTypes.COLON
+                                && rawLookup2 != BallerinaTypes.DOT
+                                && rawLookup2 != BallerinaTypes.QUOTED_STRING_LITERAL) {
                             return true;
+                        } else {
+                            return false;
                         }
-                    } while (rawLookup != null && isWhiteSpaceOrComment(rawLookup));
+                    } while (rawLookup2 != null && isWhiteSpaceOrComment(rawLookup2));
+                } else {
+                    return true;
                 }
-            }
+            } while (rawLookup != null && isWhiteSpaceOrComment(rawLookup));
         }
         return true;
     }
