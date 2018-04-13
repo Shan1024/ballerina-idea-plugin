ff424518625ca274137f804d85334cdbfb16e915
shan1024 <shanm@wso2.com>
2018-04-13 21:23:05 +0530

    Fix issue in package identifying

--- src/main/gen/org/ballerinalang/plugins/idea/parser/BallerinaParserUtil.java
+++ src/main/gen/org/ballerinalang/plugins/idea/parser/BallerinaParserUtil.java
@@ -129,9 +129,12 @@ public class BallerinaParserUtil extends GeneratedParserUtilBase {
                                 return true;
                             } else if (rawLookup == BallerinaTypes.COMMA && rawLookup2 == BallerinaTypes.ADD) {
                                 //
-                                if (latestDoneMarker != null
-                                        && latestDoneMarker.getTokenType() == BallerinaTypes.INVOCATION_ARG) {
-                                    return false;
+                                if (latestDoneMarker != null) {
+                                    IElementType tokenType = latestDoneMarker.getTokenType();
+                                    if (tokenType == BallerinaTypes.INVOCATION_ARG
+                                            || tokenType == BallerinaTypes.RECORD_KEY_VALUE) {
+                                        return false;
+                                    }
                                 }
                                 // return (variable:^"person 1".^"first name", variable2:^"person 2".^"current age2");
                                 return true;
